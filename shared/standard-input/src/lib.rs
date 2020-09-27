12c53621c5bc541a8ce7190ef15f5b750a7556c3
Arnold Gandarillas Castillo <arkgast@gmail.com>
2020-09-27 16:53:42 -0400

    feat: add standard-input crate

---
+++ shared/standard-input/src/lib.rs
@@ -0,0 +1,25 @@
+use std::io;
+use std::io::Write;
+
+pub fn read_number() -> i32 {
+    print!("Enter a number: ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    let number = parse_input_to_number(input);
+    return number;
+}
+
+fn parse_input_to_number(input_value: String) -> i32 {
+    let mut number: i32 = 0;
+    match input_value.trim().parse() {
+        Ok(parsed_number) => number = parsed_number,
+        Err(error) => {
+            eprintln!("> Error parsing number {}", error);
+            eprintln!("> {:?}", error);
+        }
+    }
+    return number;
+}
