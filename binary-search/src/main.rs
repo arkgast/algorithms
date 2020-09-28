cfce411bb83576534202c8a0bcc8f5fb39335bd1
Arnold Gandarillas Castillo <arkgast@gmail.com>
2020-09-27 23:55:48 -0400

    feat: add binary search algorithm

--- binary-search/src/main.rs
+++ binary-search/src/main.rs
@@ -1,7 +1,46 @@
 use standard_input;
 
+const VEC_LEN: i32 = 100;
+
 fn main() {
-    let number = standard_input::read_number();
-    println!("Number read: {}", number);
+    loop {
+        let number = standard_input::read_number();
+        let vec = create_vector(VEC_LEN);
+
+        if let Some((position, steps)) = binary_search(number, vec) {
+            println!("Target {} found at position {} in {} steps\n", number, position, steps);
+        } else {
+            println!("Target {} not found\n", number);
+        }
+    }
 }
 
+fn create_vector(len: i32) -> Vec<i32> {
+    let vec: Vec<i32> = (0..len).collect();
+    return vec;
+}
+
+fn binary_search(target: i32, vec: Vec<i32>) -> Option<(usize, i8)> {
+    let mut low = 0;
+    let mut high = vec.len() - 1;
+    let mut counter: i8 = 0;
+
+    while low <= high {
+        let mid = ((high - low) / 2) + low as usize;
+        let guess = vec[mid];
+        counter += 1;
+
+        if guess == target {
+            return Some((mid, counter));
+        }
+
+        if guess < target {
+            low = mid + 1;
+        }
+
+        if guess > target {
+            high = mid - 1;
+        }
+    }
+    None
+}
